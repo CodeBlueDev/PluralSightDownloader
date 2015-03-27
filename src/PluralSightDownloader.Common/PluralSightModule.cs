dcfbdb809595c8d40ff714f79cfb8e941c35acdd
CodeBlue <codebluedev@gmail.com>
2015-03-26 23:26:08 -0600

    Added URL samples to model files to know what data looks like.

--- src/PluralSightDownloader.Common/PluralSightModule.cs
+++ src/PluralSightDownloader.Common/PluralSightModule.cs
@@ -29,5 +29,103 @@
         // TODO: HasBeenViewedImageUrl
 
         // TODO: HasBeenViewedAltText
+
+        // JSON Format from url.
+        //[
+        //    {
+        //    userMayViewFirstClip: false,
+        //    moduleRef: "eric-burke/dotnet-reflector-intro",
+        //    title: "Introducing redgate .NET Reflector",
+        //    description: "This module provides a quick introduction to redgate .NET Reflector, showing you how to inspect and decompile any .NET assembly so that you can see what's going on under the hood.",
+        //    duration: "00:41:29",
+        //    hasBeenViewed: false,
+        //    isHighlighted: false,
+        //    fragmentIdentifier: "dotnet-reflector-intro",
+        //    firstClipLaunchClickHandler: "author=eric-burke&name=dotnet-reflector-intro&mode=live&clip=0&course=dotnet-reflector-by-example",
+        //    userMayBookmark: false,
+        //    isBookmarked: false,
+        //    clips: [
+        //    {
+        //    transcripts: [ ],
+        //    clipIndex: 0,
+        //    title: "Reflector part 1",
+        //    hasBeenViewed: false,
+        //    duration: "00:12:56",
+        //    playerParameters: "author=eric-burke&name=dotnet-reflector-intro&mode=live&clip=0&course=dotnet-reflector-by-example",
+        //    userMayViewClip: false,
+        //    clickActionDescription: "View with player",
+        //    isHighlighted: false,
+        //    name: "reflector-part-1",
+        //    isBookmarked: false,
+        //    hasBeenViewedImageUrl: "cs/has-not-been-viewed-checkmark-15-v1.png",
+        //    hasBeenViewedAltText: "You have not watched this Clip."
+        //    },
+        //    {
+        //    transcripts: [ ],
+        //    clipIndex: 1,
+        //    title: "Reflector part 2",
+        //    hasBeenViewed: false,
+        //    duration: "00:14:13",
+        //    playerParameters: "author=eric-burke&name=dotnet-reflector-intro&mode=live&clip=1&course=dotnet-reflector-by-example",
+        //    userMayViewClip: false,
+        //    clickActionDescription: "View with player",
+        //    isHighlighted: false,
+        //    name: "reflector-part-2",
+        //    isBookmarked: false,
+        //    hasBeenViewedImageUrl: "cs/has-not-been-viewed-checkmark-15-v1.png",
+        //    hasBeenViewedAltText: "You have not watched this Clip."
+        //    },
+        //    {
+        //    transcripts: [ ],
+        //    clipIndex: 2,
+        //    title: "Reflector part 3",
+        //    hasBeenViewed: false,
+        //    duration: "00:14:20",
+        //    playerParameters: "author=eric-burke&name=dotnet-reflector-intro&mode=live&clip=2&course=dotnet-reflector-by-example",
+        //    userMayViewClip: false,
+        //    clickActionDescription: "View with player",
+        //    isHighlighted: false,
+        //    name: "reflector-part-3",
+        //    isBookmarked: false,
+        //    hasBeenViewedImageUrl: "cs/has-not-been-viewed-checkmark-15-v1.png",
+        //    hasBeenViewedAltText: "You have not watched this Clip."
+        //    }
+        //    ],
+        //    hasBeenViewedImageUrl: "cs/has-not-been-viewed-checkmark-15-v1.png",
+        //    hasBeenViewedAltText: "You have not watched this Module."
+        //    },
+        //    {
+        //    userMayViewFirstClip: false,
+        //    moduleRef: "sahil-malik/sp2010-debugging-reflector",
+        //    title: "Debugging SharePoint 2010 With Reflector",
+        //    description: "This screencast shows you how to use Redgate's Reflector Pro Utility to debug SharePoint 2010 applications.",
+        //    duration: "00:22:50",
+        //    hasBeenViewed: false,
+        //    isHighlighted: false,
+        //    fragmentIdentifier: "sp2010-debugging-reflector",
+        //    firstClipLaunchClickHandler: "author=sahil-malik&name=sp2010-debugging-reflector&mode=live&clip=0&course=dotnet-reflector-by-example",
+        //    userMayBookmark: false,
+        //    isBookmarked: false,
+        //    clips: [
+        //    {
+        //    transcripts: [ ],
+        //    clipIndex: 0,
+        //    title: "Debugging SharePoint 2010 With Reflector",
+        //    hasBeenViewed: false,
+        //    duration: "00:22:50",
+        //    playerParameters: "author=sahil-malik&name=sp2010-debugging-reflector&mode=live&clip=0&course=dotnet-reflector-by-example",
+        //    userMayViewClip: false,
+        //    clickActionDescription: "View with player",
+        //    isHighlighted: false,
+        //    name: "c0",
+        //    isBookmarked: false,
+        //    hasBeenViewedImageUrl: "cs/has-not-been-viewed-checkmark-15-v1.png",
+        //    hasBeenViewedAltText: "You have not watched this Clip."
+        //    }
+        //    ],
+        //    hasBeenViewedImageUrl: "cs/has-not-been-viewed-checkmark-15-v1.png",
+        //    hasBeenViewedAltText: "You have not watched this Module."
+        //    }
+        //]
     }
 }
