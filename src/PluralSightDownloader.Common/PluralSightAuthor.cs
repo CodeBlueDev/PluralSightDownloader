f1765ff590d1782efa4b172c6e72812f8301f896
CodeBlue <codebluedev@gmail.com>
2015-03-26 23:29:28 -0600

    Added PluralSight Author class and CourseRating.

---
+++ src/PluralSightDownloader.Common/PluralSightAuthor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PluralSightDownloader.Common
+{
+    class PluralSightAuthor
+    {
+        // TODO: Handle
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ src/PluralSightDownloader.Common/PluralSightCourseRating.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluralSightDownloader.Common
+{
+    class PluralSightCourseRating
+    {
+        // TODO: CurrentUsersRating
+        
+        // TODO: AverageRating
+
+        // TODO: Rating
+
+        // TODO: CanRateThisCourse
+
+        // TODO: CourseName
+
+        // TODO: NumberOfRaters
+
+        // TODO: HasUserRatedCourse
+    }
+}
